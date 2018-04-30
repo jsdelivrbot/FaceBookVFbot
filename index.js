0e4821643cbd710e1b7dd582216795f4bf77f23e
marcodagolini <marco.dagolini@gmail.com>
2018-04-30 21:19:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.set('port', (process.env.PORT || 5000));
 // Required to allow access to the service across different domains
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, **Authorization**");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Content-Type', 'text/plain');
   next();
 });
