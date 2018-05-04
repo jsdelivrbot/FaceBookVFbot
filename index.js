ae105f5562a9392c16e5bb7888d2668314da4b37
marcodagolini <marco.dagolini@gmail.com>
2018-05-05 01:07:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -532,7 +532,7 @@ function closeChat(dialogID){
 										intents: [ // Last consumer message identified intents
 											{
 												id: 'NPSsent',
-												name: myTimestamp,
+												name: "myTimestamp",
 												confidenceScore: 1
 											}]
 									}];
