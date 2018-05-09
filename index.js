b9f85b11f363003530737f8571c5b138c89b39ce
marcodagolini <marco.dagolini@gmail.com>
2018-05-09 19:37:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1311,6 +1311,7 @@ function proceedWithActions(){
 								}
 							}, function (e, r, b) {
 								var arraylength = b._metadata.count;
+								console.log("isFacebook = " + isFacebook);
 								if(isFacebook < 1){
 									wasNPSsent = 1;
 									console.log("marked point 1");
