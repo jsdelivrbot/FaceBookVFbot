be138c8ef87a39dad8c2275cdbf912c2a21bdbf3
marcodagolini <marco.dagolini@gmail.com>
2018-05-13 01:29:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1537,6 +1537,7 @@ function proceedWithActions(){
 				}
 				
 				if(isToBeAwakened === "awakeLater"){
+					console.log("thisIsToBeAwakened in " + (isToBeAwakenedTimestamp - Date.now()));
 					if(isToBeAwakenedTimestamp < (Date.now())){
 						console.log("***unfreezing");
 						wakeUpChat(answer[m].info.conversationId, answer[m].info.latestAgentLoginName, channel);
