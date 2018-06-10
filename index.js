bd8350634c650667bf5117ad1ab22148da85a7c7
marcodagolini <marco.dagolini@gmail.com>
2018-06-10 15:11:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -766,7 +766,7 @@ function checkNPSwasSent(json, isFacebook, channel){
 		} else{
 								
 			for (var i = 0; i < arraylength; i++){
-				console.log(b.conversationHistoryRecords[i].info.conversationId);
+				console.log(arraylength);
 				if(b.hasOwnProperty('conversationHistoryRecords')){
 					if(b.conversationHistoryRecords[i].hasOwnProperty('transfers')){
 				
